24c5461cd2df09cd3e8786d0a5ad807e64378421
Sandro Giannini <sgianninigarcia@gmail.com>
2022-12-07 18:25:48 -0500

    Initial Commit

---
+++ FlockingBirds/FlockingBirdsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlockingBirdsApp.swift
+//  FlockingBirds
+//
+//  Created by Sandro Giannini on 12/7/22.
+//
+
+import SwiftUI
+
+@main
+struct FlockingBirdsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
